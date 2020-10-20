002ba51eeeb48a00b18b74b6ae644ab8fa32e9c1
Jinseok Seo <jiseo@student.42seoul.kr>
2020-10-20 17:59:28 +0900

    save

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jiseo <jiseo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/18 16:02:10 by jiseo             #+#    #+#             */
-/*   Updated: 2020/10/20 16:40:21 by jiseo            ###   ########.fr       */
+/*   Updated: 2020/10/20 17:15:24 by jiseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,27 @@ int		main(void)
 {
 	char	*input;
 	char	buf[MAX_PATH];
-
-	while (get_next_line(STDIN_FILENO, &input) > -1)
+	
+	while (1)
 	{
-		if (ft_strncmp(input, "pwd", 3) == 0)
-			printf("pwd[%s]\n", getcwd(buf, MAX_PATH));
-		else if (ft_strncmp(input, "cd", 2) == 0)
+		ft_putstr_fd("minishell ", STDIN_FILENO);
+		if (get_next_line(STDIN_FILENO, &input) > -1)
 		{
-			chdir("/Users/jiseo/work");
-			printf("change directory[%s]\n", getcwd(buf, MAX_PATH));
+			if (ft_strncmp(input, "pwd", 3) == 0)
+				printf("pwd[%s]\n", getcwd(buf, MAX_PATH));
+			else if (ft_strncmp(input, "cd", 2) == 0)
+			{
+				chdir("/Users/jiseo/work");
+				printf("change directory[%s]\n", getcwd(buf, MAX_PATH));
+			}
+			else if (ft_strncmp(input, "exit", 4) == 0)
+				exit(0);
+			else if (ft_strncmp(input, "./run.sh", 8) == 0)
+			{
+				execve("run.sh", NULL, NULL);
+			}
+			printf("input:[%s]\n", input);
+			free(input);
 		}
-		else if (ft_strncmp(input, "exit", 4) == 0)
-			exit(0);
-		printf("input:[%s]\n", input);
-		free(input);
 	}
 }
