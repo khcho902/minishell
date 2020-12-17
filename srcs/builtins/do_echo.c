7341fd64801f2f86b716ba072c93e302dfe1411b
jiseo <jiseo@student.42seoul.kr>
2020-12-17 14:37:24 +0900

    export, echo done

--- srcs/builtins/do_echo.c
+++ srcs/builtins/do_echo.c
@@ -6,7 +6,7 @@
 /*   By: jiseo <jiseo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/25 21:51:59 by jiseo             #+#    #+#             */
-/*   Updated: 2020/12/17 12:52:01 by jiseo            ###   ########.fr       */
+/*   Updated: 2020/12/17 13:58:39 by jiseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,22 @@
 void		do_echo(t_msh *msh, int fd)
 {
 	int		idx;
+	int		n_option;
 	
-	idx = 1;
+	if (msh->cmds->length > 1)
+	{
+		if (!ft_strcmp(msh->cmds->args[1], "-n"))
+			n_option = 1;
+		else
+			n_option = 0;
+	}
+	idx = 1 + n_option;
 	while (idx < msh->cmds->length)
 	{
 		ft_putstr_fd(msh->cmds->args[idx], fd);
 		ft_putchar_fd(' ', fd);
 		idx++;
 	}
-	ft_putchar_fd('\n', fd);
+	if (!n_option)
+		ft_putchar_fd('\n', fd);
 }
--- srcs/builtins/do_export.c
+++ srcs/builtins/do_export.c
@@ -6,7 +6,7 @@
 /*   By: jiseo <jiseo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 04:22:01 by jiseo             #+#    #+#             */
-/*   Updated: 2020/12/17 13:02:12 by jiseo            ###   ########.fr       */
+/*   Updated: 2020/12/17 14:14:04 by jiseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,20 +73,20 @@ static t_dict	**export_env(t_msh *msh, t_dict **dest)
 	int			idx;
 
 	dest = copy_env(msh, dest);
-	idx = msh->env_len;
+	idx = 1;
 	while (idx < msh->cmds->length)
 	{
 		arg = msh->cmds->args[idx];
 		chr = ft_strchr(arg, '=');
-		if (!(dest[idx] = (t_dict *)malloc(sizeof(t_dict))))
+		if (!(dest[msh->env_len + idx - 1] = (t_dict *)malloc(sizeof(t_dict))))
 			exit_print_err(strerror(errno));
-		if (!(dest[idx]->key = ft_substr(arg, 0, chr - arg)))
+		if (!(dest[msh->env_len + idx - 1]->key = ft_substr(arg, 0, chr - arg)))
 			exit_print_err(strerror(errno));
-		if (!(dest[idx]->value = ft_strdup(chr + 1)))
+		if (!(dest[msh->env_len + idx - 1]->value = ft_strdup(chr + 1)))
 			exit_print_err(strerror(errno));
 		idx++;
 	}
-	dest[idx] = NULL;
+	dest[msh->env_len + msh->cmds->length - 1] = NULL;
 	return (dest);
 }
 
