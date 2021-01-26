60f731408256792b7b19bb9ed52313e96d50ad2a
khcho902 <khcho902@gmail.com>
2021-01-26 22:50:08 +0900

    change logo!!! && finish!!!

--- srcs/prompt/prompt.c
+++ srcs/prompt/prompt.c
@@ -15,15 +15,28 @@
 void		show_logo(void)
 {
 	ft_putstr_fd(COLOR_BLUE, STDOUT);
-	ft_putstr_fd("        :::      ::::::::\n", STDOUT);
-	ft_putstr_fd("      :+:      :+:    :+:\n", STDOUT);
-	ft_putstr_fd("    +:+ +:+         +:+  \n", STDOUT);
-	ft_putstr_fd("  +#+  +:+       +#+	   \n", STDOUT);
-	ft_putstr_fd("+#+#+#+#+#+   +#+        \n", STDOUT);
-	ft_putstr_fd("     #+#    #+#          \n", STDOUT);
-	ft_putstr_fd("    ###   ########.kr    \n", STDOUT);
+	ft_putstr_fd(
+		"███╗   ███╗██╗███╗   ██╗██╗███████╗██╗  ██╗███████╗██╗     ██╗     \n",
+		STDOUT);
+	ft_putstr_fd(
+		"████╗ ████║██║████╗  ██║██║██╔════╝██║  ██║██╔════╝██║     ██║     \n",
+		STDOUT);
+	ft_putstr_fd(
+		"██╔████╔██║██║██╔██╗ ██║██║███████╗███████║█████╗  ██║     ██║     \n",
+		STDOUT);
+	ft_putstr_fd(
+		"██║╚██╔╝██║██║██║╚██╗██║██║╚════██║██╔══██║██╔══╝  ██║     ██║     \n",
+		STDOUT);
+	ft_putstr_fd(
+		"██║ ╚═╝ ██║██║██║ ╚████║██║███████║██║  ██║███████╗███████╗███████╗\n",
+		STDOUT);
+	ft_putstr_fd(
+		"╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝\n",
+		STDOUT);
 	ft_putstr_fd(COLOR_BR_GREEN, STDOUT);
-	ft_putstr_fd("by. kycho, jiseo\n", STDOUT);
+	ft_putstr_fd(
+		"                                                   by. kycho, jiseo\n",
+		STDOUT);
 }
 
 void		show_prompt(void)
